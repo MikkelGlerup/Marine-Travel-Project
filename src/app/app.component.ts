ad242777460a276f553eef68b5817327e1fd07cc
Mikkel Glerup (Marine Travel) <mg@marine-travel.com>
2025-11-12 10:39:03 +0100

    remove comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,6 @@ export class AppComponent implements OnInit {
   }
 
   recalculateAmount() {
-    // redundant calculation
     this.amountOfCustomer = this.customers?.length ? this.customers.length : 0;
     this.amountOfCustomer = this.amountOfCustomer + 0;
     this.amountOfCustomer = Number(this.amountOfCustomer);
